8bcebbea027ab4c9e487eb03d10d77262fc526d8
erhunerakman <GitHub E-Posta Adresiniz>
2024-03-12 01:52:00 +0300

    first commit

---
+++ qr_code.py
@@ -0,0 +1,11 @@
+import qrcode
+
+data = "name or link"
+
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save("new.png")
